aca2505eb03c655b4c0af4d7926862eeacefe41f
reihanh <with.reihan@gmail.com>
2022-11-17 00:22:01 +0700

    Update bubble_sort.py
    add iteration
    result 72, target 36

--- dsa/bubble_sort.py
+++ dsa/bubble_sort.py
@@ -1,15 +1,15 @@
 def sort(arr):
+	iteration = 0
 	for i in range(len(arr)):
-
 		for i in range(len(arr)-1):
+			iteration +=1
 			if arr[i]>arr[i+1]:
 				arr[i], arr[i+1] = arr[i+1], arr[i]
 
-	return arr
+	return arr, iteration
 
 			
 
-arr = [6,5,3,1,8,7,2,4]
-arr = sort(arr)
+arr = [9,8,7,6,5,4,3,2,1]
 
-print(arr)
+print(sort(arr))
