37162f726eb304f449562d5f7c36fbe79eb38ddb
reihanh <with.reihan@gmail.com>
2022-11-17 00:00:07 +0700

    Update binary_search_recursion.py

--- recursion/binary_search_recursion.py
+++ recursion/binary_search_recursion.py
@@ -8,16 +8,9 @@ def search(arr, target, start, end):
 		elif (arr[mid] < target):
 			search(arr,target, mid+1, end)
 		else:
-			return mid
+			print(mid)
 	else:
-		return -1
+		print("not found")
 
 arr = [2, 4, 8, 9, 12, 15, 18, 41, 81]
-target = 7
-
-result = search(arr,target, 0, len(arr)-1)
-
-if result != -1:
-	print(f"element on index {result}")
-else:
-	print("element not found")
+target = 81
